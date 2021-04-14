e86c76e11300459cf378be414ef4da2216627f8a
nurik-kr <njumabekov6@gmail.com>
2021-04-14 11:57:20 +0600

    fetch back stack

--- app/src/main/java/kg/nurik/finalproject/ui/bottomNav/allGames/AllGamesFragment.kt
+++ app/src/main/java/kg/nurik/finalproject/ui/bottomNav/allGames/AllGamesFragment.kt
@@ -1,10 +1,11 @@
 package kg.nurik.finalproject.ui.bottomNav.allGames
 
-import android.graphics.Color
 import android.os.Bundle
+import android.system.Os.remove
 import android.view.View
 import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
@@ -40,16 +41,15 @@ class AllGamesFragment : Fragment(R.layout.fragment_all_games) {
     private fun checkIsInternet() {
         val isHasNetwork = ConnectionUtils.isNetworkAvailable(requireContext())
         if (!isHasNetwork) {
-            val snack = Snackbar.make(parentLayout, "Проверьте наличие интернета",
+            Snackbar.make(
+                parentLayout, "Проверьте наличие интернета",
                 Snackbar.LENGTH_INDEFINITE
             )
                 .setAction("Обновить") {
                     if (!ConnectionUtils.isNetworkAvailable(requireContext()))
                         checkIsInternet()
                 }
-            snack.setTextColor(Color.parseColor("#FF000000"))
-            snack.setActionTextColor(Color.parseColor("#FF000000"))
-            snack.show()
+                .show()
         }
     }
 
